350567def8af6876c388e55c0268f4b6a07488fd
Ar-Adunakhor <camilosamuelmedranomartinez@gmail.com>
2024-10-25 17:12:17 -0600

    Cambio a Maven desde Ant
    Uso de FlatLaf para el aspecto visual

---
+++ src/main/java/com/proyecto/sic115/sistema/SIC115Sistema.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.proyecto.sic115.sistema;
+import my.login.Login;
+/**
+ *
+ * @author Grupo 8
+ */
+public class SIC115Sistema {
+
+    public static void main(String[] args) {
+        Login login = new Login();
+        login.show();
+    }
+}
